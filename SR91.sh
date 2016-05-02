6e90fc8cb9233b312af265c53b266edcbfe9d135
deruci <deruci@snu.ac.kr>
2016-05-02 16:25:54 +0900

    modified

--- SR91.sh
+++ SR91.sh
@@ -3,6 +3,7 @@
 echo "Download SR91 training dataset for super-resolution to /data/SR91"
 wget "https://www.dropbox.com/s/xnwn4o7m6fwtoa3/SR91_PNG.zip"
 mkdir data
-mkdir data/SR91
-unzip SR91_PNG.zip -d data/SR91
+mkdir data/training
+mkdir data/training/SR91
+unzip SR91_PNG.zip -d data/training/SR91
 rm -f SR91_PNG.zip
